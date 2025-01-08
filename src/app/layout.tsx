a57ecd49bcf3c9f7d0964db23c85162a3f52e71a
reZerOR <gkraihan14k@gmail.com>
2025-01-08 23:22:50 +0600

    theme added and boiler plate removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className={`${lexend.variable} antialiased font-lexend`}>
         <ThemeProvider
           attribute={"class"}
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
