156bc0c3c93a1ac7c56fe5926433c1ae15c588f2
reZerOR <gkraihan14k@gmail.com>
2025-01-09 23:15:37 +0600

    icon added in banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import { SparklesText } from "@/components/ui/sparkles-text";
+import { QrCode } from "lucide-react";
 
 export default function Home() {
   return (
     <div>
-      <div className="text-center">
-        <SparklesText text="Generate QRs" />
+      <div>
+        <div className="flex flex-col justify-center items-center gap-4 my-20">
+          <QrCode size={56} className="text-green-500" />
+          <SparklesText text="Generate QRs" />
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </div>
   );
