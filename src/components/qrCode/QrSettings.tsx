c626e0ca1d7a3cb7f7b61c27e38d655ead1443a0
reZerOR <gkraihan14k@gmail.com>
2025-02-03 23:35:26 +0600

    ui modified with icons and settings tab added

--- src/components/qrCode/QrSettings.tsx
+++ src/components/qrCode/QrSettings.tsx
@@ -2,13 +2,15 @@ import { TabsContent } from "@radix-ui/react-tabs";
 import { Tabs, TabsList, TabsTrigger } from "../ui/tabs";
 import QrType from "./QrTypes/QrTypes";
 import QrImage from "./QrImageOptions/QrImage";
+import { ImageIcon, QrCode, Settings } from "lucide-react";
 
 const QrSettings = () => {
   return (
     <Tabs defaultValue="qrType" className="w-[400px] space-y-4">
-      <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="qrType">Qr Types</TabsTrigger>
-        <TabsTrigger value="imageOptions">Image Options</TabsTrigger>
+      <TabsList className="grid w-full grid-cols-3">
+        <TabsTrigger className="gap-2" value="qrType"><QrCode/>Types</TabsTrigger>
+        <TabsTrigger className="gap-2" value="imageOptions"><ImageIcon/>Options</TabsTrigger>
+        <TabsTrigger className="gap-2" value="advanced"><Settings/>Settings</TabsTrigger>
       </TabsList>
       <TabsContent value="qrType">
         <QrType/>
